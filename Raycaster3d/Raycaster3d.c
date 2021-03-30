fa799c73936daafd29996e996fd1bc3cfeddfebf
dweller05 <49336871+dweller05@users.noreply.github.com>
2021-03-31 00:36:20 +0900

    Add files via upload

---
+++ Raycaster3d/Raycaster3d.c
@@ -0,0 +1,192 @@
+#include <stdlib.h>
+#include <GL/glut.h>
+#include <math.h>
+
+typedef struct
+{
+	int w,a,s,d;
+}ButtonKeys; ButtonKeys Keys;
+
+#define mapX  8      //map width
+#define mapY  8      //map height
+#define mapS 64      //map cube size
+int map[]=           //the map array. Edit to change level but keep the outer walls
+{
+ 1,1,1,1,1,1,1,1,
+ 1,0,0,0,0,0,0,1,
+ 1,0,1,0,0,0,0,1,
+ 1,0,1,0,0,0,0,1,
+ 1,0,0,0,0,0,0,1,
+ 1,0,0,0,0,1,0,1,
+ 1,0,0,0,0,0,0,1,
+ 1,1,1,1,1,1,1,1,
+};
+
+void drawMap2D()
+{
+ int x,y,xo,yo;
+ for(y=0;y<mapY;y++)
+ {
+  for(x=0;x<mapX;x++)
+  {
+   if(map[y*mapX+x]==1){ glColor3f(1,1,1);} else{ glColor3f(0,0,0);}
+   xo=x*mapS; yo=y*mapS;
+   glBegin(GL_QUADS); 
+   glVertex2i( 0   +xo+1, 0   +yo+1); 
+   glVertex2i( 0   +xo+1, mapS+yo-1); 
+   glVertex2i( mapS+xo-1, mapS+yo-1);  
+   glVertex2i( mapS+xo-1, 0   +yo+1); 
+   glEnd();
+  } 
+ } 
+}
+
+float degToRad(float a) { return a*M_PI/180.0;}
+float FixAng(float a){ if(a>359){ a-=360;} if(a<0){ a+=360;} return a;}
+
+float px,py,pdx,pdy,pa;
+
+void drawPlayer2D()
+{
+ glColor3f(1,1,0);   glPointSize(8);    glLineWidth(4);
+ glBegin(GL_POINTS); glVertex2i(px,py); glEnd();
+ glBegin(GL_LINES);  glVertex2i(px,py); glVertex2i(px+pdx*20,py+pdy*20); glEnd();
+}
+
+float distance(ax,ay,bx,by,ang){ return cos(degToRad(ang))*(bx-ax)-sin(degToRad(ang))*(by-ay);}
+
+void drawRays2D()
+{
+ glColor3f(0,1,1); glBegin(GL_QUADS); glVertex2i(526,  0); glVertex2i(1006,  0); glVertex2i(1006,160); glVertex2i(526,160); glEnd();	
+ glColor3f(0,0,1); glBegin(GL_QUADS); glVertex2i(526,160); glVertex2i(1006,160); glVertex2i(1006,320); glVertex2i(526,320); glEnd();	 	
+	
+ int r,mx,my,mp,dof,side; float vx,vy,rx,ry,ra,xo,yo,disV,disH; 
+ 
+ ra=FixAng(pa+30);                                                              //ray set back 30 degrees
+ 
+ for(r=0;r<60;r++)
+ {
+  //checks vertical 
+  dof=0; side=0; disV=100000;
+  float Tan=tan(degToRad(ra));
+       if(cos(degToRad(ra))> 0.001){ rx=(((int)px>>6)<<6)+64;      ry=(px-rx)*Tan+py; xo= 64; yo=-xo*Tan;}//looking left
+  else if(cos(degToRad(ra))<-0.001){ rx=(((int)px>>6)<<6) -0.0001; ry=(px-rx)*Tan+py; xo=-64; yo=-xo*Tan;}//looking right
+  else { rx=px; ry=py; dof=8;}                                                  //looking up or down. no hit  
+
+  while(dof<8) 
+  { 
+   mx=(int)(rx)>>6; my=(int)(ry)>>6; mp=my*mapX+mx;                     
+   if(mp>0 && mp<mapX*mapY && map[mp]==1){ dof=8; disV=cos(degToRad(ra))*(rx-px)-sin(degToRad(ra))*(ry-py);}//hit         
+   else{ rx+=xo; ry+=yo; dof+=1;}                                               //check next horizontal
+  } 
+  vx=rx; vy=ry;
+
+  //checks horizontal
+  dof=0; disH=100000;
+  Tan=1.0/Tan; 
+       if(sin(degToRad(ra))> 0.001){ ry=(((int)py>>6)<<6) -0.0001; rx=(py-ry)*Tan+px; yo=-64; xo=-yo*Tan;}//looking up 
+  else if(sin(degToRad(ra))<-0.001){ ry=(((int)py>>6)<<6)+64;      rx=(py-ry)*Tan+px; yo= 64; xo=-yo*Tan;}//looking down
+  else{ rx=px; ry=py; dof=8;}                                                   //looking straight left or right
+ 
+  while(dof<16) 
+  { 
+   mx=(int)(rx)>>6; my=(int)(ry)>>6; mp=my*mapX+mx;                          
+   if(mp>0 && mp<mapX*mapY && map[mp]==1){ dof=16; disH=cos(degToRad(ra))*(rx-px)-sin(degToRad(ra))*(ry-py);}//hit         
+   else{ rx+=xo; ry+=yo; dof+=1;}                                               //check next horizontal
+  } 
+  
+  glColor3f(0,0.8,0);
+  if(disV<disH){ rx=vx; ry=vy; disH=disV; glColor3f(0,0.6,0);}                  //horizontal hit first
+  glLineWidth(2); glBegin(GL_LINES); glVertex2i(px,py); glVertex2i(rx,ry); glEnd();//draw 2D ray
+    
+  int ca=FixAng(pa-ra); disH=disH*cos(degToRad(ca));                            //fix fisheye 
+  int lineH = (mapS*320)/(disH); if(lineH>320){ lineH=320;}                     //line height and limit
+  int lineOff = 160 - (lineH>>1);                                               //line offset
+  
+  glLineWidth(8);glBegin(GL_LINES);glVertex2i(r*8+530,lineOff);glVertex2i(r*8+530,lineOff+lineH);glEnd();//draw vertical wall  
+
+  ra=FixAng(ra-1);                                                              //go to next ray
+ }
+}
+
+
+void init()
+{
+ glClearColor(0.3,0.3,0.3,0);
+ gluOrtho2D(0,1024,510,0);
+ px=150; py=400; pa=90;
+ pdx=cos(degToRad(pa)); pdy=-sin(degToRad(pa)); 
+}
+
+float frame1,frame2,fps;
+
+void display()
+{  
+ //frames per second
+ frame2=glutGet(GLUT_ELAPSED_TIME); fps=(frame2-frame1); frame1=glutGet(GLUT_ELAPSED_TIME);
+ 
+ //buttons
+ if(Keys.a==1){ pa+=0.2*fps; pa=FixAng(pa); pdx=cos(degToRad(pa)); pdy=-sin(degToRad(pa));} 	
+ if(Keys.d==1){ pa-=0.2*fps; pa=FixAng(pa); pdx=cos(degToRad(pa)); pdy=-sin(degToRad(pa));} 
+ 
+ int xo=0; if(pdx<0){xo=-20;} else{xo=20;}
+ int yo=0; if(pdy<0){yo=-20;} else{yo=20;}
+ int ipx=px/64.0, ipx_add_xo=(px+xo)/64.0, ipx_sub_xo=(px-xo)/64.0;
+ int ipy=py/64.0, ipy_add_yo=(py+yo)/64.0, ipy_sub_yo=(py-yo)/64.0;
+
+ if(Keys.w==1)
+ {
+  if(map[ipy*mapX+ipx_add_xo]==0){px+=pdx*0.2*fps;}
+  if(map[ipy_add_yo*mapX + ipx]==0){py+=pdy*0.2*fps;} 
+ }
+ if(Keys.s==1)
+ {
+  if(map[ipy*mapX+ipx_sub_xo]==0){px-=pdx*0.2*fps;}
+  if(map[ipy_sub_yo*mapX + ipx]==0){py-=pdy*0.2*fps;} 
+ }
+ glutPostRedisplay();
+  
+ glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+ drawMap2D();
+ drawPlayer2D();
+ drawRays2D();
+ glutSwapBuffers();  
+}
+
+void ButtonDown(unsigned char key, int x,int y)
+{
+	if(key=='a'){Keys.a=1;}
+	if(key=='d'){Keys.d=1;}
+	if(key=='w'){Keys.w=1;}
+	if(key=='s'){Keys.s=1;}
+	glutPostRedisplay();
+}
+
+void ButtonUp(unsigned char key, int x,int y)
+{
+	if(key=='a'){Keys.a=0;}
+	if(key=='d'){Keys.d=0;}
+	if(key=='w'){Keys.w=0;}
+	if(key=='s'){Keys.s=0;}
+	glutPostRedisplay();
+}
+
+void resize(int w,int h)
+{
+	glutReshapeWindow(1024,512);
+}
+
+int main(int argc, char* argv[])
+{ 
+ glutInit(&argc, argv);
+ glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+ glutInitWindowSize(1024,510);
+ glutInitWindowPosition(200,200);
+ glutCreateWindow("OpenGL Raytracing");
+ init();
+ glutDisplayFunc(display);
+ glutReshapeFunc(resize);
+ glutKeyboardFunc(ButtonDown);
+ glutKeyboardUpFunc(ButtonUp);
+ glutMainLoop();
+}
